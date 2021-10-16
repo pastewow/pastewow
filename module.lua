ed836bfacde0d2bf59eb6d68c5b36c9a5d83c9c0
paste.wow <92420501+pastewow@users.noreply.github.com>
2021-10-16 03:18:05 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,3 +1,1 @@
-game.ReplicatedStorage.MainEvent:FireServer("CHECKER")
-game.ReplicatedStorage.MainEvent:FireServer("TeleportDetect")
-game.ReplicatedStorage.MainEvent:FireServer("OneMoreTime")
+print('b')
